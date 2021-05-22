2e29f5b5e4e1ee80bcb985105465704c33238ebe
Danny Cho <dannycho7@gmail.com>
2021-05-22 05:04:42 +0000

    Add utils/plots.py

---
+++ utils/plots.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_loss_and_accuracy_curves(model_name, loss, acc, loss_v, acc_v):
+    plt.figure()
+    epochs = np.arange(20)+1
+    plt.plot(epochs, loss, label='{} - training'.format(model_name))
+    plt.plot(epochs, loss_v, label='{} - validation'.format(model_name))
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.title("Pretrained {} Loss".format(model_name))
+    plt.legend()
+    plt.show()
+    plt.figure()
+    plt.plot(epochs, acc, label='{} - training'.format(model_name))
+    plt.plot(epochs, acc_v, label='{} - validation'.format(model_name))
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    plt.title("Pretrained {} Accuracy".format(model_name))
+    plt.legend()
+    plt.show()
